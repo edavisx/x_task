ed788a353ffcc3ede18ad86d0f14cb5d021df71e
Enrique Davis <114179772+edavisx@users.noreply.github.com>
2025-02-06 15:32:19 +0100

    Delete zzz directory

--- zzz/cerrar_session.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset(); // Eliminar todas las variables de sesión
-session_destroy(); // Destruir la sesión
-
-// Redirigir al usuario a la página de login
-header("Location: login.php");
-exit();
-?>
